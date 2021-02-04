e8caec827dd7fc01abbd3a4a69bd6e91a398da7f
Mark Rivera <Mriver1991@gmail.com>
2021-02-04 13:04:08 -0500

    created state, methods to change state, computed data points, set up watch to search for changes in state

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,49 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">@{{ user.username }} - {{ fullName }}</div>
+  <strong>Followers: </strong> {{ followers }}
+  <button @click="followUser">Follow</button>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  components: {},
+  data() {
+    return {
+      followers: 0,
+      user: {
+        id: 1,
+        username: "_MarkRivera",
+        fname: "Mark",
+        lname: "Rivera",
+        email: "Mrivera1991@gmail.com",
+        isAdmin: true,
+      },
+    };
+  },
+
+  watch: {
+    followers(newFollowerCount, oldFollowerCount) {
+      oldFollowerCount < newFollowerCount &&
+        console.log(`${this.user.username} has gained a follower!`);
+    },
+  },
+
+  computed: {
+    fullName() {
+      return `${this.user.fname} ${this.user.lname}`;
+    },
+  },
+  methods: {
+    followUser() {
+      this.followers++;
+    },
+  },
+
+  mounted() {
+    this.followUser();
+  },
+};
 </script>
 
 <style>
@@ -19,8 +51,9 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
 }
 </style>
